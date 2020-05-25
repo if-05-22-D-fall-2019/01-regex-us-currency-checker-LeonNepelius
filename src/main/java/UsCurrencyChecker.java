a6da0b775690075fceb5642d481a550e7abe73d8
LeonNepelius <leon@nepelius.at>
2020-05-25 09:06:27 +0200

    Implemented getMatcher TODO: Regex

--- src/main/java/UsCurrencyChecker.java
+++ src/main/java/UsCurrencyChecker.java
@@ -1,9 +1,9 @@
-import sun.reflect.generics.reflectiveObjects.NotImplementedException;
-
 import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class UsCurrencyChecker {
     public static Matcher getMatcher(String usCurrencyString) {
-        throw new NotImplementedException();
+        Pattern pattern = Pattern.compile(""); //TODO regex
+        return pattern.matcher(usCurrencyString);
     }
 }
