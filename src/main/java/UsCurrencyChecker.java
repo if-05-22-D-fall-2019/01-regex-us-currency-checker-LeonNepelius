03790ad3439839c1eb23ef566de70ca71cfcdb1e
AdventureT <le@nepelius.at>
2020-05-25 09:28:14 +0200

    Regex done

--- src/main/java/UsCurrencyChecker.java
+++ src/main/java/UsCurrencyChecker.java
@@ -1,9 +1,9 @@
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-public class UsCurrencyChecker {
-    public static Matcher getMatcher(String usCurrencyString) {
-        Pattern pattern = Pattern.compile(""); //TODO regex
+class UsCurrencyChecker {
+    static Matcher getMatcher(String usCurrencyString) {
+        Pattern pattern = Pattern.compile("\\$\\s?\\d+(\\.\\d{1,2})?");
         return pattern.matcher(usCurrencyString);
     }
 }
